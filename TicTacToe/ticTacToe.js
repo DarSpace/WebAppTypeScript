0661bbc8770cc44d1d6d1a9061464f59b80270b4
DarSpace <45310234+DarSpace@users.noreply.github.com>
2020-03-31 22:46:17 +0200

    TicTacToe v1.0.0

--- TicTacToe/ticTacToe.js
+++ TicTacToe/ticTacToe.js
@@ -5,22 +5,30 @@ var Players;
 })(Players || (Players = {}));
 var Board = /** @class */ (function () {
     function Board(buttonInputs) {
-        this.currentPlayer = Players.Player1;
-        this.movesBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
-        this.buttonInputs = buttonInputs;
-    }
-    Board.prototype.listenForClicks = function () {
         var _this = this;
-        buttonInputs.forEach(function (elem, index) {
-            elem.addEventListener("click", function (event) {
-                _this.movesBoard[index] = _this.currentPlayer;
-                elem.value = _this.currentPlayer;
-                elem.disabled = true;
+        this.currentPlayer = Players.Player1;
+        this.cells = [
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell(),
+            new Cell()
+        ];
+        this.buttonInputs = buttonInputs;
+        this.buttonInputs.forEach(function (elem, index) {
+            console.log(elem);
+            _this.cells[index] = new Cell();
+            elem.addEventListener("click", function (v) {
+                _this.cells[index].onClick(_this.currentPlayer, elem);
+                _this.winner();
                 _this.updateCurrentPlayer();
-                console.log(_this.movesBoard);
             });
         });
-    };
+    }
     Board.prototype.updateCurrentPlayer = function () {
         if (this.currentPlayer === Players.Player1) {
             this.currentPlayer = Players.Player2;
@@ -29,14 +37,50 @@ var Board = /** @class */ (function () {
             this.currentPlayer = Players.Player1;
         }
     };
+    Board.prototype.winner = function () {
+        console.log(this.currentPlayer);
+        if ((this.cells[0].playerId == this.currentPlayer &&
+            this.cells[1].playerId == this.currentPlayer &&
+            this.cells[2].playerId == this.currentPlayer) ||
+            (this.cells[3].playerId == this.currentPlayer &&
+                this.cells[4].playerId == this.currentPlayer &&
+                this.cells[5].playerId == this.currentPlayer) ||
+            (this.cells[6].playerId == this.currentPlayer &&
+                this.cells[7].playerId == this.currentPlayer &&
+                this.cells[8].playerId == this.currentPlayer) ||
+            (this.cells[0].playerId == this.currentPlayer &&
+                this.cells[3].playerId == this.currentPlayer &&
+                this.cells[6].playerId == this.currentPlayer) ||
+            (this.cells[1].playerId == this.currentPlayer &&
+                this.cells[4].playerId == this.currentPlayer &&
+                this.cells[7].playerId == this.currentPlayer) ||
+            (this.cells[2].playerId == this.currentPlayer &&
+                this.cells[5].playerId == this.currentPlayer &&
+                this.cells[8].playerId == this.currentPlayer) ||
+            (this.cells[0].playerId == this.currentPlayer &&
+                this.cells[4].playerId == this.currentPlayer &&
+                this.cells[8].playerId == this.currentPlayer) ||
+            (this.cells[2].playerId == this.currentPlayer &&
+                this.cells[4].playerId == this.currentPlayer &&
+                this.cells[6].playerId == this.currentPlayer)) {
+            if (this.currentPlayer == "X")
+                alert("Win Player 1");
+            else
+                alert("Win Player 2");
+        }
+    };
     return Board;
 }());
-var Game = /** @class */ (function () {
-    function Game(buttonInputs) {
-        var board = new Board(buttonInputs);
-        board.listenForClicks();
+var Cell = /** @class */ (function () {
+    function Cell() {
+        this.playerId = "";
     }
-    return Game;
+    Cell.prototype.onClick = function (playerChar, button) {
+        this.playerId = playerChar;
+        button.value = playerChar;
+        button.disabled = true;
+    };
+    return Cell;
 }());
 var buttonInputs = document.querySelectorAll(".inputButton");
-var game = new Game(buttonInputs);
+var game = new Board(buttonInputs);
--- TicTacToe/ticTacToe.ts
+++ TicTacToe/ticTacToe.ts
@@ -1,30 +1,39 @@
-enum Players {
+enum Players { //  przypisujemy wartości dla playerów X i O
   Player1 = "X",
   Player2 = "O"
 }
 
 class Board {
-  currentPlayer: Players = Players.Player1;
+  currentPlayer: Players = Players.Player1; // pobranie playera 1 z enuma
+  // tworzenie tablicy wszystkich pol
+  cells = [
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell(),
+    new Cell()
+  ];
 
-  movesBoard: string[] = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
   buttonInputs: NodeListOf<HTMLInputElement>;
 
   constructor(buttonInputs: NodeListOf<HTMLInputElement>) {
     this.buttonInputs = buttonInputs;
-  }
-
-  listenForClicks() {
-    buttonInputs.forEach((elem: HTMLInputElement, index: number) => {
-      elem.addEventListener("click", event => {
-        this.movesBoard[index] = this.currentPlayer;
-        elem.value = this.currentPlayer;
-        elem.disabled = true;
+    this.buttonInputs.forEach((elem: HTMLInputElement, index: number) => {
+      console.log(elem);
+      this.cells[index] = new Cell();
+      elem.addEventListener("click", v => {
+        this.cells[index].onClick(this.currentPlayer, elem);
+        this.winner();
         this.updateCurrentPlayer();
-        console.log(this.movesBoard);
       });
     });
   }
 
+  // zmiana tury graczy
   updateCurrentPlayer() {
     if (this.currentPlayer === Players.Player1) {
       this.currentPlayer = Players.Player2;
@@ -32,16 +41,55 @@ class Board {
       this.currentPlayer = Players.Player1;
     }
   }
-}
 
-class Game {
-  constructor(buttonInputs: NodeListOf<HTMLInputElement>) {
-    const board: Board = new Board(buttonInputs);
-    board.listenForClicks();
+  // sprawdzanie warunków wygranej
+  winner() {
+    console.log(this.currentPlayer);
+    if (
+      (this.cells[0].playerId == this.currentPlayer &&
+        this.cells[1].playerId == this.currentPlayer &&
+        this.cells[2].playerId == this.currentPlayer) ||
+      (this.cells[3].playerId == this.currentPlayer &&
+        this.cells[4].playerId == this.currentPlayer &&
+        this.cells[5].playerId == this.currentPlayer) ||
+      (this.cells[6].playerId == this.currentPlayer &&
+        this.cells[7].playerId == this.currentPlayer &&
+        this.cells[8].playerId == this.currentPlayer) ||
+      (this.cells[0].playerId == this.currentPlayer &&
+        this.cells[3].playerId == this.currentPlayer &&
+        this.cells[6].playerId == this.currentPlayer) ||
+      (this.cells[1].playerId == this.currentPlayer &&
+        this.cells[4].playerId == this.currentPlayer &&
+        this.cells[7].playerId == this.currentPlayer) ||
+      (this.cells[2].playerId == this.currentPlayer &&
+        this.cells[5].playerId == this.currentPlayer &&
+        this.cells[8].playerId == this.currentPlayer) ||
+      (this.cells[0].playerId == this.currentPlayer &&
+        this.cells[4].playerId == this.currentPlayer &&
+        this.cells[8].playerId == this.currentPlayer) ||
+      (this.cells[2].playerId == this.currentPlayer &&
+        this.cells[4].playerId == this.currentPlayer &&
+        this.cells[6].playerId == this.currentPlayer)
+    ) {
+      if (this.currentPlayer == "X") alert("Win Player 1");
+      else alert("Win Player 2");
+    }
   }
 }
 
+class Cell {
+  playerId = ""; // wartosc na pustym polu
+
+  // klikniecie w przycisk
+  onClick(playerChar: string, button: HTMLInputElement) {
+    this.playerId = playerChar;
+    button.value = playerChar;
+    button.disabled = true;
+  }
+}
+
+// pobieranie pola 3x3
 const buttonInputs: NodeListOf<HTMLInputElement> = document.querySelectorAll(
   ".inputButton"
 );
-const game = new Game(buttonInputs);
+const game = new Board(buttonInputs); // tworzenie planszy
